04ffeed54b9b8d9a5bb2adfb7a89f1ba79dd98bb
Pawel Alessandro Reynoso Marquez <pawelreynoso@gmail.com>
2022-04-02 22:17:19 -0600

    primer cambio

---
+++ Alumno.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationLab8
+{
+    public class Alumno
+    {
+        public string carne { get; set; }
+        public string nombre { get; set; }
+    }
+}
